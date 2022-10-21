6020fdf386a349bdb9decd09bd9696e938516121
shubham sharma <shubham00sharma0@gmail.com>
2022-10-21 16:24:08 +0530

    Crud added

--- base/views.py
+++ base/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room,Topic,Message
+from .forms import RoomForm
 
-from django.http import HttpResponse
 
 def home(request):
-    return  render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return  render(request, 'base/home.html',context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
 
 
+def createRoom(request):
+    form = RoomForm
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request ,'base/room_form.html',context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(pk)
+    return redirect

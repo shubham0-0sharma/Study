5e0b4b4595ec4cdd2e3b3c7fd223b821a42eb5cc
shubham sharma <shubham00sharma0@gmail.com>
2022-12-19 20:50:36 +0530

    changed staticfiles settings

--- discord/settings.py
+++ discord/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR/'staticfiles'
 STATIC_URL = "static/"
 MEDIA_URL = "images/"
-MEDIA_ROOT =  BASE_DIR/'staticfiles/images'
+MEDIA_ROOT =  BASE_DIR/'static/images'
+STATIC_DIRS = [BASE_DIR/'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

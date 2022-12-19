1051f4d2e5c4f11d3e31333f7a6a23f4a020b2db
shubham sharma <shubham00sharma0@gmail.com>
2022-12-19 20:10:42 +0530

    changed settings

--- discord/settings.py
+++ discord/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,11 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = "/static/"
 MEDIA_URL = "images/"
 STATICFILES_DIRS = [ BASE_DIR / 'static']
-MEDIA_ROOT = BASE_DIR/'static/images'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
